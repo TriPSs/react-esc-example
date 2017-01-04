9f41d1109cdcf2e8ea63e70662cd3fb454803005
Tycho Bokdam <t.bokdam@seronaris.nl>
2017-01-04 21:03:25 +0100

    Update package so css will be rendered

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -4,22 +4,23 @@ import { Provider } from 'react-redux'
 import Helmet from 'react-helmet'
 
 class AppContainer extends React.Component {
+
   static propTypes = {
-    layout: PropTypes.object,
-    history: PropTypes.object.isRequired,
-    routes: PropTypes.object.isRequired,
+    layout   : PropTypes.object,
+    history  : PropTypes.object.isRequired,
+    routes   : PropTypes.object.isRequired,
     routerKey: PropTypes.number,
-    store: PropTypes.object.isRequired
+    store    : PropTypes.object.isRequired
   }
 
-  render () {
-    const { layout, history, routes, routerKey, store } = this.props
+  render() {
+    const {layout, history, routes, routerKey, store} = this.props
 
     return (
       <Provider store={store}>
-        <div style={{ height: '100%' }}>
+        <div style={{height: '100%'}}>
           <Helmet {...layout} />
-          <Router history={history} children={routes} key={routerKey} />
+          <Router history={history} children={routes} key={routerKey}/>
         </div>
       </Provider>
     )
